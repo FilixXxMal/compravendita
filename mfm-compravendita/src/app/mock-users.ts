d16a7536106d5105155f4583628838087a333ea8
FilixXxMal <maldifassi.filippo.maria@iisgalvanimi.edu.it>
2019-05-29 12:55:09 +0000

    punto 2.1

--- mfm-compravendita/src/app/mock-users.ts
+++ mfm-compravendita/src/app/mock-users.ts
@@ -1,7 +1,7 @@
 import { User } from './user';
 
 export const MockUsers: User[] = [
-  { nome: 'filixmal',  email: 'aldifassi.filippo@iisgalvanimi.edu.it', password: 'googlegalvani' },
-  { nome: 'danyvolp',  email: 'volpato.daniele@iisgalvanimi.edu.it',   password: 'danylollipop'  },
-  { nome: 'bellux',    email: 'bellia.matteo@iisgalvanimi.edu.it',     password: 'sonbellux'     }
+  { nome: 'filixmal',  email: 'maldifassi.filippo@iisgalvanimi.edu.it', password: 'IlovePucca' },
+  { nome: 'danyvolp',  email: 'volpato.daniele@iisgalvanimi.edu.it',    password: 'danylollipop'  },
+  { nome: 'bellux',    email: 'bellia.matteo@iisgalvanimi.edu.it',      password: 'sonbellux'     }
 ];
